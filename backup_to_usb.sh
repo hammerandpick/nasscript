ba4a3596843daa58a579838bd89241b796794e62
⚒️ <42745482+hammerandpick@users.noreply.github.com>
2020-01-17 16:23:41 +0100

    Update and rename backup_to_usb to backup_to_usb.sh

--- backup_to_usb.sh
+++ backup_to_usb.sh
@@ -4,5 +4,5 @@ ENCDATA=/volumeUSB1/usbshare/encrypted/
 SOURCE=/volume2/snapshots/
 BACKUP=/volumeUSB1/usbshare/backup_no_enc/
 
-sudo mount -t ecryptfs $ENCDATA $ZIEL
+sudo mount -t ecryptfs $ENCDATA $BACKUP
 rsync -az -H --progress --delete --numeric-ids $SOURCE $BACKUP
