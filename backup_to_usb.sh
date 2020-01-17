71b3c2372851572a318ee2617ab040038978af0e
⚒️ <42745482+hammerandpick@users.noreply.github.com>
2020-01-17 16:28:14 +0100

    Update backup_to_usb.sh

--- backup_to_usb.sh
+++ backup_to_usb.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# requirements: you have a usb drive mounted to the given mount points
+# this drive is encrypted with ecryptfs
+
+# 1. script will mount encypted data folder to a mount point
+# 2. script will start a backup with rsync. the links are verified so you may safely copy a structre with multiple links to a file
+
 ENCDATA=/volumeUSB1/usbshare/encrypted/
 SOURCE=/volume2/snapshots/
 BACKUP=/volumeUSB1/usbshare/backup_no_enc/
