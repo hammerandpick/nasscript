7d84442abbb7685c8762d1c0cda2ad62fd0c0d4b
⚒️ <42745482+hammerandpick@users.noreply.github.com>
2020-02-02 11:28:16 +0100

    added parameter -f
    -f for full copy.

--- backup_to_usb.sh
+++ backup_to_usb.sh
@@ -11,10 +11,10 @@ SOURCE=/volume2/snapshots/
 BACKUP=/volumeUSB1/usbshare/backup_no_enc/
 
 sudo mount -t ecryptfs $ENCDATA $BACKUP
-# if your pc has enough memory
-rsync -az -H --progress --delete --numeric-ids $SOURCE $BACKUP
+
 # if your pc has not enough memory
-# and folders are like daily.0/ daily.1/ ... 
+# and folders are like daily.0/ daily.1/ ...
+
 if [ $# -eq 0 ] || [ "$1" = "-d" ] || [ "$1" = "--daily" ] ; then
   echo "copy daily.*"
   rsync -az -H --progress --delete --numeric-ids --include='daily.*/***' --exclude='*' $SOURCE $BACKUP
@@ -29,6 +29,7 @@ fi
 
 if [ $# -eq 0 ] || [ "$1" = "-m" ] || [ "$1" = "--monthly" ] ; then
   echo "copy monthly.*"
+  # ATTENTION refering to weekly (eg. in month ) may not work, since rsync will perform operation in alphabetical order
   rsync -az -H --progress --delete --numeric-ids --include='daily.6/***' --include='monthly.*/***' --exclude='*' $SOURCE $BACKUP
   else echo "skip monthly."
 fi
@@ -37,4 +38,11 @@ if [ $# -eq 0 ] || [ "$1" = "-y" ] || [ "$1" = "--yearly" ] ; then
   echo "copy yearly.*"
   rsync -az -H --progress --delete --numeric-ids --include='daily.6/***' --include='weekly.3' --include='monthly.11/***' --include='yearly.*/***' --exclude='*' $SOURCE $BACKUP
   else echo "skip yearly."
+fi
+
+if [ "$1" = "-f" ] || [ "$1" = "--full" ] ; then
+  echo "performing full copy."
+  # if your pc has enough memory
+  rsync -az -H --progress --delete --numeric-ids $SOURCE $BACKUP
+  else "skipping full copy."
 fi
